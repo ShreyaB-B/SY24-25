f872c779deae33a0d090d0b6ae300ffd014145f0
Shreya Benham <slben401@learner.dvusd.org>
2024-12-03 14:11:51 -0700

    WPF_Dex Project
    Have to finish the textbox information

---
+++ WPF_Dex/WPF_Dex/MainWindow.xaml.vb
@@ -0,0 +1,28 @@
+﻿Class MainWindow
+    Private Sub Nighthawk_Click(sender As Object, e As RoutedEventArgs)
+        PictureBox1.Visibility = Visibility.Visible
+        F15Egale.Visibility = Visibility.Hidden
+        B2Bomber.Visibility = Visibility.Hidden
+        Rec.Visibility = Visibility.Hidden
+        Label1.Content = "B-2 Bomber"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Bomber_Click(sender As Object, e As RoutedEventArgs)
+        PictureBox1.Visibility = Visibility.Hidden
+        F15Egale.Visibility = Visibility.Visible
+        B2Bomber.Visibility = Visibility.Hidden
+        Rec.Visibility = Visibility.Hidden
+        Label1.Content = "F-15 Eagle"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Eagle_Click(sender As Object, e As RoutedEventArgs)
+        PictureBox1.Visibility = Visibility.Hidden
+        F15Egale.Visibility = Visibility.Hidden
+        B2Bomber.Visibility = Visibility.Visible
+        Rec.Visibility = Visibility.Hidden
+        Label1.Content = "F-117 NightHawk"
+        TextBox1.Text = ""
+    End Sub
+End Class
